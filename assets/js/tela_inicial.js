539e501cf59f2fd65fb24112c68e41751c893a39
luiz-brancher <lppbrancher@senacrs.com.br>
2023-03-30 11:24:40 -0300

    tela inicial

---
+++ assets/js/tela_inicial.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    if(localStorage.getItem('lstvideo-intro')!=='true'){
+        $('html').css('overflow', 'hidden')
+        $('.backdrop').fadeIn()
+      } 
+      $('.botao-video').click(function(){
+        $('.backdrop').fadeIn()
+        $('html').css('overflow', 'hidden')
+      })
+      $('#fechar-video').click(function(){
+        $('.backdrop').hide()
+        localStorage.setItem('lstvideo-intro', 'true')
+    })
+});
